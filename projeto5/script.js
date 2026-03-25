6b1498e50084315efeee1ce52f671e85f584e950
João Almeida <joaoalmeida0913@outlook.com>
2026-03-25 10:24:06 -0300

    Projeto 5 finalizado

---
+++ projeto5/script.js
@@ -0,0 +1,22 @@
+const btn = document.getElementById('btn');
+let min, max,correction, result;
+
+function random (min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+btn.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    min = parseInt(document.getElementById('minimo').value);
+    max = parseInt(document.getElementById('maximo').value);
+    if (min > max) {
+        correction = min;
+        min = max;
+        max = correction;
+    }
+
+    result = random(min, max)
+    document.getElementById('resultado').innerHTML = result;
+})
